ca6c0f890b0a3aa814dc047b204ab6160460f4ea
Badal Chouhan <badalsinghchouhan564@gmail.com>
2024-09-10 21:31:14 +0530

    day four

---
+++ react-project-four/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+ // create state
+ // change state
+ // manage state
+ // sabhi childe me state sync karwadunga
+
+ const [name, setName] = useState('');
+  return (
+   <div>
+    <Card title="Card1" name = {name} setName = {setName}/>
+    <Card title="Card2" name = {name} setName = {setName}/>
+    {/* <p>I am inside Parent component and value of name is: {name}</p> */}
+   </div>
+  )
+}
+
+export default App
